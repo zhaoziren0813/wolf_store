7d057d3d02eb35cb052e9fb7a81afcb89153ab66
赵梓荏的Mac <Mac>
2017-08-18 11:01:40 +0800

    Initial commit

---
+++ src/main/java/com/wolf/WolfStoreApplication.java
@@ -0,0 +1,12 @@
+package com.wolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolfStoreApplication.class, args);
+	}
+}
